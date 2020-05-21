989ad546cd09405e77843f8c7ff9462fb9f5edc4
佐毅 <yaoyajie@enmonster.com>
2020-05-21 12:21:02 +0800

    最长子串问题整理

--- 算法时空/数组/leetcode-01两数之和/main.cpp
+++ 算法时空/数组/leetcode-01两数之和/main.cpp
@@ -35,7 +35,7 @@ public:
         }
         
         for (int i = 0; i < size; ++i){
-            for (int j = i + 1; j < size; ++j){
+            for (int j = i + 1; j < size; j++){
                 if ((nums[i] + nums[j]) == target){
                     result.push_back(i);
                     result.push_back(j);
